e297099bd9216251fac6a16125ac2f83e532a3e5
Oybek <oybekganiyev722@gmail.com>
2026-09-05 18:25:19 +0500

    New files have been added.
    1chi dars qisqacha yuklandi

---
+++ 1 dars.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+#"""
+#Spyder Editor
+
+#This is a temporary script file.
+#"""
+#print( "Hello World")
+#
+#print("O'ngacha sanaymiz")
+#for n in range(10):
+ # print(n+1) 
+
+#son=input("Istalgan son kiriting:")
+#son=int(son)
+#print(f"{son} ning kvadrati {son**2} ga teng")
+son=float(input("Istalgan son kiriting:"))
+if son>=0:
+    print("musbat son")
+else:
+    print("manfiy son")    
+    
