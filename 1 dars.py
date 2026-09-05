1e725590cf313b530d49422c9ebd639451e11f7d
Oybek <oybekganiyev722@gmail.com>
2026-09-05 18:30:27 +0500

    1-dars yangilandi

--- 1 dars.py
+++ 1 dars.py
@@ -10,9 +10,9 @@
 #for n in range(10):
  # print(n+1) 
 
-#son=input("Istalgan son kiriting:")
-#son=int(son)
-#print(f"{son} ning kvadrati {son**2} ga teng")
+son=input("Istalgan son kiriting:")
+son=int(son)
+print(f"{son} ning kvadrati {son**2} ga teng")
 son=float(input("Istalgan son kiriting:"))
 if son>=0:
     print("musbat son")
